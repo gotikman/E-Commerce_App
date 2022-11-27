4ff38e05fa3f7c0af3cec474e493917a054824b5
gotikman <vydyvnyk@gmail.com>
2022-11-27 14:21:05 +0200

    fix deploy

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -37,7 +37,8 @@ export default async function handler(req, res) {
                     }
                 }),
                 success_url: `${req.headers.origin}/success`,
-                cancel_url: `${req.headers.origin}/canceled`,
+                // cancel_url: `${req.headers.origin}/canceled`,
+                cancel_url: `${req.headers.origin}/`,
             }
 
             // Create Checkout Sessions from body params.
