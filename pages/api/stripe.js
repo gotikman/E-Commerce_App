7a18d15e59c536473b92564ac94e9276a65a799d
gotikman <vydyvnyk@gmail.com>
2022-12-02 00:06:14 +0200

    +banner, +payment method

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -12,8 +12,9 @@ export default async function handler(req, res) {
                 payment_method_types: ['card'],
                 billing_address_collection: 'auto',
                 shipping_options: [
-                    { shipping_rate: 'shr_1M8ZgUGczNMdQTxtdNQ06cFe' },
-                    { shipping_rate: 'shr_1M8a1QGczNMdQTxtymSAaofp' },
+                    { shipping_rate: 'shr_1MAJk5GczNMdQTxtMI2THSbW' },
+                    { shipping_rate: 'shr_1MAJoYGczNMdQTxtePJPFNHR' },
+
 
                 ],
                 line_items: req.body.map((item) => {
--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ const Home = ({ products, bannerData }) => (
       {products?.map((product) => <Product key={product._id} product={product} />)}
     </div>
 
-    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+    <FooterBanner footerBanner={bannerData && bannerData[1]} />
   </div>
 );
 
